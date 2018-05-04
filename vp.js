ad7b603b4aba1ef5152c1c9c7428b7df0052dea6
Abishai <Abishai2007@gmail.com>
2018-05-04 12:35:55 +1000

    Changed Google ID

--- vp.js
+++ vp.js
@@ -1,7 +1,7 @@
 function vp_main($scope)
 {
 	var gAccount = new AuthAccount();
-	gAccount.authClientID = '304094492573-vgmnc6pr5tf1va809qpoc6sri6amiptk.apps.googleusercontent.com';  // vp
+	gAccount.authClientID = '604779244106-2altleqgj8a1nibdst2dbr5gqjdrv7ok.apps.googleusercontent.com';  // vp
 	gAccount.authScope = 'https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/drive.appdata';
 
 	var gAppData = new AuthAppData();
